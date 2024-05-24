282123c6359c1086472c8e72a33b37b6859c3f49
VadimTrubay <vadnetvadnet@ukr.net>
2024-05-24 09:47:15 +0300

    Update contacts.js

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,8 +1,32 @@
 import { ContactsCollection } from '../db/contact.js';
+import { calculatePaginationData } from '../utils/calculatePaginationData.js';
+import { SORT_ORDER } from '../constants/constantsApp.js';
 
-export const getAllContacts = async () => {
-  const contacts = await ContactsCollection.find();
-  return contacts;
+export const getAllContacts = async ({
+                                       page = 1,
+                                       perPage = 10,
+                                       sortOrder = SORT_ORDER.ASC,
+                                       sortBy = '_id',
+                                     }) => {
+  const limit = perPage;
+  const skip = (page - 1) * perPage;
+
+  const contactsQuery = ContactsCollection.find();
+  const contactsCount = await ContactsCollection.find()
+    .merge(contactsQuery)
+    .countDocuments();
+
+  const contacts = await contactsQuery
+    .skip(skip)
+    .limit(limit)
+    .sort({ [sortBy]: sortOrder })
+    .exec();
+  const paginationData = calculatePaginationData(contactsCount, perPage, page);
+
+  return {
+    data: contacts,
+    ...paginationData,
+  };
 };
 
 export const getContactById = async (contactId) => {
@@ -13,7 +37,7 @@ export const getContactById = async (contactId) => {
 export const createContact = async (payload) => {
   const contact = await ContactsCollection.create(payload);
   return contact;
-}
+};
 
 export const updateContact = async (contactId, payload, options = {}) => {
   const rawResult = await ContactsCollection.findOneAndUpdate(
@@ -34,9 +58,9 @@ export const updateContact = async (contactId, payload, options = {}) => {
   };
 };
 
-export  const deleteContact = async (contactId) => {
+export const deleteContact = async (contactId) => {
   const contact = await ContactsCollection.findOneAndDelete({
     _id: contactId,
   });
   return contact;
-}
+};
